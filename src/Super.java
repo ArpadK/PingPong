22f6ad078806e4dfddb55202c47381cf8617f80b
Arpad Kolkert <arpad.kolkert@student.hu.nl>
2017-05-12 14:28:16 +0100

    Pull members up

--- src/Super.java
+++ src/Super.java
@@ -1,7 +1,32 @@
+import javax.swing.*;
+
 /**
  * Created by arko1 on 12/05/2017.
  */
 public class Super {
+    static int screenSize = 300;    //and width - screen is square
+    static int paddleSize = 25;     //Actually half the paddle size - how much to draw on each side of center
+    static int paddleDistanceFromSide = 10;  //How much space between each paddle and side of screen
+    static int computerPaddleY = screenSize / 2 ;    //location of the center of the paddles on the Y-axis of the screen
+    static int humanPaddleY = screenSize / 2 ;
+    static int computerPaddleMaxSpeed = 3;   //Max number of pixels that computer paddle can move clock tick. Higher number = easier for computer
+    static int humanPaddleMaxSpeed = 5;   //This doesn't quite do the same thing... this is how many pixels human moves per key press TODO use this in a better way
+    static int humanPaddleSpeed = 0;      // "speed" is pixels moved up or down per clock tick
+    static int computerPaddleSpeed = 0;   // same
+    static double  ballX = (double) screenSize / 2;   //Imagine the ball is in a square box. These are the coordinates of the top of that box.
+    static double  ballY = (double) screenSize / 2;   //So this starts the ball in (roughly) the center of the screen
+    static int ballSize = 10;                //Diameter of ball
+    static double ballSpeed = 5;   //Again, pixels moved per clock tick
+    //An angle in radians (which range from 0 to 2xPI (0 to about 6.3).
+    //This starts the ball moving down toward the human. Replace with some of the other
+    //commented out versions for a different start angle
+    //set this to whatever you want (but helps if you angle towards a player)
+    static double ballDirection = Math.PI + 1;   //heading left and up
+    static Timer timer;    //Ticks every *gameSpeed* milliseconds. Every time it ticks, the ball and computer paddle move
+
+    static boolean gameOver;      //Used to work out what message, if any, to display on the screen
+    static boolean removeInstructions = false;  // Same as above
+
     public Super(){
 
     }
