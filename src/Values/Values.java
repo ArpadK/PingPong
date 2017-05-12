33539180b5c7ea2b52be508997118ff346af3a72
Arpad Kolkert <arpad.kolkert@student.hu.nl>
2017-05-12 15:01:47 +0100

    Undo factory method to do builder

--- src/Values/Values.java
+++ src/Values/Values.java
@@ -5,14 +5,10 @@ public class Values {
     private final int b;
     private final int c;
 
-    private Values(int a, int b, int c) {
+    public Values(int a, int b, int c) {
         this.a = a;
         this.b = b;
         this.c = c;
-    }
-
-    public static Values createValues(int a, int b, int c) {
-        return new Values(a, b, c);
     }
 
     public int getA() {
